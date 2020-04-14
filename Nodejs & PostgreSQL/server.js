9820605f75fb4427e6388c3ac46b7a3d65d12a2e
suriya <suriya.m@bambeeq.com>
2020-04-14 11:18:43 +0530

    deployed

---
+++ Nodejs & PostgreSQL/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+ 
+const cors = require('cors')
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+ 
+const db = require('./app/config/db.config.js');
+  
+// force: true will drop the table if it already exists
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync with { force: true }');
+  initial();
+});
+ 
+require('./app/route/customer.route.js')(app);
+ 
+// Create a Server
+var server = app.listen(8080, function () {
+ 
+  let host = server.address().address
+  let port = server.address().port
+ 
+  console.log("App listening at http://%s:%s", host, port);
+})
+ 
+function initial(){
+ 
+  let customers = [
+    {
+      submit: "Is There any Update",
+    }
+  ]
+ 
+  // Init data -> save to MySQL
+  const Customer = db.customers;
+  for (let i = 0; i < customers.length; i++) { 
+    Customer.create(customers[i]);  
+  }
+}
